35c209008c832d5b296f1ecf5adc9768b4cce66f
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2024-09-15 10:36:17 -0300

    update litte changes

--- src/components/SectionNextWelcome.jsx
+++ src/components/SectionNextWelcome.jsx
@@ -4,7 +4,7 @@ const SectionNextWelcome = () => {
   return (
     <section className='w-full px-0 py-0 my-0'>
     <div className="space-y-2 p-6 text-center text-base bg-secundary">
-      <h1 className="font-bold text-xl text-third text-2xl">
+      <h1 className="font-bold text-third text-2xl">
       Fabricamos las máquinas que la industria necesita
       </h1>
       <p className="text-white px-6 pb-2">
